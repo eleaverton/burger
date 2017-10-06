52d1732ddf91e89ccd3fd1edf93f51f25e9ae3e1
Liz Leaverton <egleaverton@gmail.com>
2017-10-06 09:35:07 -0500

    had to delete functioning repository so I could add .gitignore and add node modules to it

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole Burger",false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lamb Burger",false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon CheeseBurger",false);
